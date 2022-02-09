a71bb11e704ffac477eec29b382d89a44b3bf9ea
mks124rus <mks124rus@gmail.com>
2022-02-09 15:39:02 +0700

    Add Project

---
+++ SwiftUI_VK/SwiftUI_VK/SwiftUI_VKApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VKApp.swift
+//  SwiftUI_VK
+//
+//  Created by Максим Валюшко on 09.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
